1b6ddc0a0bfa45dedc4156dd645df25fca158f90
alswo0202 <64340415+alswo0202@users.noreply.github.com>
2021-03-01 15:43:03 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+//const button = document.querySelectorAll("button"); // 버튼 태그 불러오기 
+//const element = document.getElementsByClassName("element");
+
+
+
+function onClickBUtton(number){
+    alert(number)
+}
+
+
+}
